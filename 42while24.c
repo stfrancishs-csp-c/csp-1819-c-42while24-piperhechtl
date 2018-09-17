2492ad1e8ec1af676cb572e33c8cc29e4ad9071f
Piper S. Hechtl <19hechtl@stfrancishs.org>
2018-09-17 18:50:44 +0000

    42while24

--- 42while24.c
+++ 42while24.c
@@ -3,20 +3,25 @@ int main(void)
 {
     int sum=0;
     int k;
+    //for (k=0;k<5;k++)
+    k = 0;
 
-    for (k=0;k<5;k++)
+    while(k<5)
     {
-        sum+=k; //use something different in "while" version
+
+        sum+=k;//use something different in "while" version
+        k++;
     }
 
     printf ("%d\n",sum);
 
     k=5;
 
-    while (k>0) {
-        k=k-1; //use something different in "for" version
+    //while (k>0) {
+    for(k=4;k>0;k--)
+{
+       // k=k-1; //use something different in "for" version
         printf ("%d\n", k);
     }
-
 }
 
